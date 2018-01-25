cb78467c07e1208770d8cbe0ceee133be0591fb2
tijanadjorovic <tijana89djorovic@gmail.com>
2018-01-25 12:24:09 +0100

    templates,html

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-"""Flask app."""
+"""Flask APP."""
 
 from flask import Flask
-from models import Comment
 from flask import render_template
+from flask import jsonify
+
+from models import COMMENTS
 
 app = Flask(__name__)
+
 
 @app.route("/")
 # def index():
 #     print "Welcome!"
 
-@app.route("/comments")
+@app.route('/comments')
 def comments():
-    user = [
-        Comment('a','1111.11.11'),
-        Comment('b','2222.22.22'),
-        Comment('c','3333.33.33')
-]
-    return render_template('index.html', title='Comments', user=user, )
+    return render_template('index.html', data=COMMENTS)
 
 
-
-# @app.route("/api/v1.0/comments")
-# def api_comments
+@app.route('/api/v1.0/comments', methods=['GET'])
+def api_comments():
+    result = []
+    for comment in COMMENTS:
+        comment_dict = {
+            "TEXT": comment.text,
+            "DATE": comment.date
+        }
+        result.append(comment_dict)
+    return jsonify({'comments': result})
--- models.py
+++ models.py
@@ -7,40 +7,10 @@ class Comment(object):
         self.date = date
 
     def __repr__(self):
-        return "text: {}, date: {}".format(
-            self.text,
-            self.date
-    )
+        """Return string represantation of the object of type Comment."""
+        return "Comment(text={}, date={})".format(self.text, self.date)
 
-    def tr(self):
-        """Return user as HTML table row."""
-        tr = "<tr>"
-        tr += "<td>{}</td>".format(self.text)
-        tr += "<td>{}</td>".format(self.date)
-        tr += "</tr>"
-        return tr
-
-# class ComTable(Table):
-#     text = Col('text')
-#     date = Col('date')
-
-# class Comment(object):
-#     def __init__(self, text, date):
-#         self.text = text
-#         self.date = date
-
-# COMMENTS=[
-#     Comment('sanja','1991.24.01'),
-#     Comment('tijana','1989.03.12'),
-#     Comment('ceca','2018.15.07')
-# ]
-
-# table = ComTable(Comment)
-
-# print(table.__html__())
-
-if __name__ == '__main__':
-    COMMENTS=[
+COMMENTS=[
         Comment('a','1111.11.11'),
         Comment('b','2222.22.22'),
         Comment('c','3333.33.33')
